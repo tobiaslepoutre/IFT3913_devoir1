36500ed6d3f5feec132c1e2fda9f6f05bc190af5
Tobias Lepoutre <115952383+tobiaslepoutre@users.noreply.github.com>
2023-10-06 06:27:38 -0400

    Update Tls.java

--- Tls.java
+++ Tls.java
@@ -1,1 +1,78 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.regex.Pattern;
 
+public class tls {
+
+    public static void main(String[] args) throws IOException {
+        if (args.length < 1) {
+            System.out.println("Format: tls [-o <chemin-à-la-sortie.csv>] <chemin-de-l'entrée>");
+            return;
+        }
+
+        String outputPath = null;
+        String inputPath;
+
+        if (args[0].equals("-o")) {
+            if (args.length < 3) {
+                System.out.println("Il manque le Chemin d'entrée parmis les arguments");
+                return;
+            }
+            outputPath = args[1];
+            inputPath = args[2];
+        } else {
+            inputPath = args[0];
+        }
+
+        File inputDir = new File(inputPath);
+        if (!inputDir.exists() || !inputDir.isDirectory()) {
+            System.out.println("Ce chemin d'entrée n'existe pas ou n'est pas un répertoire");
+            return;
+        }
+
+        StringBuilder output = new StringBuilder();
+        output.append("Chemin du fichier, Nom du paquet, Nom de la classe, Tloc de la classe, Tassert de la classe, Tcmp de la classe\n");
+
+        for (File file : inputDir.listFiles()) {
+            if (file.getName().endsWith(".java")) {
+                String content = new String(Files.readAllBytes(file.toPath()));
+                String packageName = extractPackageName(content);
+                String className = extractClassName(file.getName());
+                int val_tloc = tloc.calculateTloc(file);
+                int val_tassert = tassert.calculateTassert(file);
+                double tcmp = val_tloc / (double) val_tassert;
+
+                output.append(file.getPath()).append(", ")
+                      .append(packageName).append(", ")
+                      .append(className).append(", ")
+                      .append(val_tloc).append(", ")
+                      .append(val_tassert).append(", ")
+                      .append(tcmp).append("\n");
+            }
+        }
+
+        if (outputPath != null) {
+            try (FileWriter fw = new FileWriter(new File(outputPath))) {
+                fw.write(output.toString());
+            }
+        } else {
+            System.out.println(output);
+        }
+    }
+
+    private static String extractPackageName(String content) {
+        Pattern pattern = Pattern.compile("package\\s+([^;]+);");
+        java.util.regex.Matcher matcher = pattern.matcher(content);
+        if (matcher.find()) {
+            return matcher.group(1).trim();
+        }
+        return "";
+    }
+
+    private static String extractClassName(String fileName) {
+        return fileName.replace(".java", "");
+    }
+
+}
